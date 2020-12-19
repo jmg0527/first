986a80ef9451259481c3b5daffc7c73367dffe99
Geong Min Gyo <jmg000527@gmail.com>
2020-12-19 16:00:47 +0900

    초기 커밋

---
+++ number.py
@@ -0,0 +1,15 @@
+my_name = 'python'
+my_age = 25
+
+print(my_name, '은 이제', my_age, '살')
+
+my_next_age = my_age + 1
+
+print('내년에는', my_next_age, '살')
+
+multiply = 9 * 9 # 81
+divide = 30 /5 # 6
+power = 2 ** 10 # 1024
+reminder = 15 % 4 # 3
+
+print(multiply, divide, power, reminder)
