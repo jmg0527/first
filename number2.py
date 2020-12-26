dec5a9e09ab994b5285bf404b9d0dc157d621e24
Geong Min Gyo <jmg000527@gmail.com>
2020-12-27 00:18:21 +0900

    study

--- number2.py
+++ number2.py
@@ -6,3 +6,23 @@ print(five)
 print(five2)
 print(five3)
 
+
+five4 = 5 * 1
+five5 = 5 * 1.0
+
+print(five4)
+print(five5)
+
+div1 = 6 / 5
+div2 = 6 // 5
+
+print(div1)
+print(div2)
+
+a=6
+b=5
+print( a == b * (a//b) + (a % b))
+
+print(int(5.0))
+print(float(5))
+print(5 * 1.0)
